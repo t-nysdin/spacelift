b17101f789de4f7dedaacc9740bbb679ad537fae
t-nysdin <133645883+t-nysdin@users.noreply.github.com>
2023-05-16 07:35:53 +0900

    change ssm.tf

--- ssm.tf
+++ ssm.tf
@@ -1,7 +1,7 @@
 resource "aws_ssm_parameter" "spacelift_test" {
   type  = "String"
   name = "/spacelift/test"
-  value = "test"
+  value = "test__hoge"
 }
 
 resource "aws_ssm_parameter" "spacelift_test2" {
